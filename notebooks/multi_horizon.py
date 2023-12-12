2ef891a64749fbe2a6beb7a3cf56959d76302734
RAYMOND LAM (DSCOE-ISD-OOCLL/HKG) <LAMRA2@OOCL.COM>
2023-12-12 23:53:42 +0800

    updated function name of multi model approach in Prediction class

--- notebooks/multi_horizon.py
+++ notebooks/multi_horizon.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 import pandas as pd
 
@@ -33,7 +34,7 @@ class Prediction:
 
         return np.array(valid_y_pred)
 
-    def multi_model_approach(
+    def multi_model_train_predict(
         df_item_id: pd.DataFrame,
         names: list,
         windowsize: int,
@@ -114,7 +115,7 @@ class Prediction:
 
         return df_y, df_y_pred, list_lgbm_model
 
-    def multi_model_approach_exo_features(
+    def multi_model_train_predict_exo_features(
         df_item_id: pd.DataFrame,
         df_calendar: pd.DataFrame,
         df_prices: pd.DataFrame,
@@ -227,4 +228,100 @@ class Prediction:
         df_y_pred = pd.DataFrame(np.array(y_pred_concat), columns=names)
 
         return df_y, df_y_pred, list_lgbm_model
+
+    def multi_model_predict_exo_features(
+        df_item_id: pd.DataFrame,
+        df_calendar: pd.DataFrame,
+        df_prices: pd.DataFrame,
+        names: list,
+        list_loaded_model: list,
+        exo_num_cols: list = [],
+        exo_cat_cols: list = [],
+    ):
+        """
+        Multi model approach - 1 model for 1 timestep prediction
+        (tabularise data + set train valid test + prediction + concat prediction)
+        i.e.
+        - Prediction(t+1) = model_1(obs(t), obs(t-1), …, obs(t-w))
+        - Prediction(t+2) = model_2(obs(t), obs(t-1), …, obs(t-w))
+        - ...
+        - Prediction(t+n) = model_n(obs(t), obs(t-1), …, obs(t-w))
+
+        df_item_id          : a pd.DataFrame containing all item_id lvl time series
+        df_calendar         : calendar.csv
+        df_prices           : sell_prices.csv then agg to item_id lvl
+        names               : a list of item_id name (str), len(names)=1 -> single ts
+        list_loaded_model   : a sorted list of loaded lgbm models (step1-28 model)
+        exo_cat_cols        : e.g. ['weekday', 'holiday'] or []
+        exo_num_cols        : e.g. ['sell_price'] or []
+        """
+
+        model = list_loaded_model[0]
+        windowsize = max([int(re.findall(r'\d+', feature_name)[0]) for feature_name in model.feature_name()])
+        horizon = len(list_loaded_model)
+        exo_num_cols = []
+        exo_cat_cols = []
+
+        # Return a df_y for validation
+        df_y = (
+            df_item_id[df_item_id.index.isin(names)]
+            .T.iloc[-horizon:][names]
+            .reset_index(drop=True)
+        )
+
+        y_pred_concat = []
+
+        for step in np.arange(1, horizon + 1):
+            print(f"---------------------Step{step}---------------------")
+            df_m = TsHandler.create_training_data(
+                df_item_id, names, windowsize, horizon, step=step
+            )
+                    
+            # update index = index of y{step}
+            df_m.index = df_m.index + windowsize + step - 1
+
+            # join calendar & price
+            df_m = df_m.merge(
+                df_calendar[["weekday_encoded", "holiday_encoded", "wm_yr_wk"]],
+                left_index=True,
+                right_index=True,
+                how="left",
+            )
+
+            # preserve the index for prediction
+            df_m = df_m.reset_index().set_index(['item_id', 'wm_yr_wk'])
+
+            df_m = df_m.merge(
+                df_prices.set_index(['item_id', 'wm_yr_wk']),
+                left_index=True,
+                right_index=True,
+                how="left",
+            )
+
+            # resume the index
+            df_m = df_m.reset_index().set_index("index")
+
+            # sort df_m by names
+            df_m = df_m.sort_values(by='item_id', key=lambda x: x.map({name: i for i, name in enumerate(names)}))
+
+            assert df_m.columns.isin(["weekday_encoded", "holiday_encoded", "sell_price"]).sum() == 3, "Error: join calendar & price!"
+
+            # Create lgbm dataset for lgbm training
+            num_cols = [f"x{i+1}" for i in range(windowsize)] + exo_num_cols
+            target_col = f"y{step}"
+            dataset_all = Dataset(
+                df_m, cat_cols=exo_cat_cols, num_cols=num_cols, target_col=target_col
+            )
+
+            # Single time step prediction
+            # Select the step th row of features in VALID for each item_id
+            idx = min(df_m[df_m.train_valid_test=='VALID'].index) + step - 1
+            features = dataset_all.num_cols + dataset_all.cat_encoded_cols
+            input = df_m.loc[df_m.index == idx, features]
+            y_pred = list_loaded_model[step-1].predict(input)  #shape:(len(names),)
+            y_pred_concat.append(y_pred)        #len:step
+
+        df_y_pred = pd.DataFrame(np.array(y_pred_concat), columns=names)
+
+        return df_y, df_y_pred
     
